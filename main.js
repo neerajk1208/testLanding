dabdc659b9880aa4dff9b1d5c6c4e8a9b0d50b32
Neeraj Kohirkar <neerajk1208@gmail.com>
2015-10-10 15:11:18 -0700

    initiated javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+var carrot = document.querySelector(".scroll");
+var columns = document.querySelector(".thirdSection");
+
+carrot.addEventListener("click", function() {
+	columns.className = "thirdSection";
+
+})
